20b9e5980660ab5a8ee733c54697bcf644f1209d
Abdou Mandara <abdoumandara@gmail.com>
2026-07-16 17:25:44 +0100

    Feature : Mise en place de la partie de modification des projets

--- taches/urls.py
+++ taches/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.liste_des_projets, name="Affichage_des_projets"),
-    path('creation', views.creation_projet, name='Creation_de_projet')
+    path('creation', views.creation_projet, name='Creation_de_projet'),
+    path('mise_a_jour/<int:projet_id>', views.modification_projet, name='Mise_a_jour_du_projet')
 ]
--- taches/views.py
+++ taches/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Projet
+from .forms import ProjetForm
+from django.contrib import messages
+
 # Create your views here.
 
 def liste_des_projets(request) :
     projets = Projet.objects.all()
     return render(request, 'templates_projets/list_projets.html',{'projets' : projets})
+
+def creation_projet(request) :
+    if request.method == 'POST' :
+        form = ProjetForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            form = ProjetForm()
+            messages.success(request, 'Projet créé avec succès !')
+            return redirect('Affichage_des_projets')
+        else :
+            messages.error(request, 'Erreur lors de la création du projet')
+    else :
+            form = ProjetForm()
+    return render(request,'templates_projets/create_projets.html', {'form' : form} )
+
+
+def modification_projet(request, projet_id) :
+    obj = get_object_or_404(Projet, id=projet_id)
+    if request.method == 'POST' :
+        form = ProjetForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            form = ProjetForm()
+            messages.success(request, 'Projet mis à jour avec succès !')
+            return redirect('Affichage_des_projets')
+        else :
+            messages.error(request, 'Erreur lors de la mise à jour du projet')
+    else :
+            form = ProjetForm(instance=obj)
+    return render(request,'templates_projets/update_projets.html', {'form' : form} )
+            
